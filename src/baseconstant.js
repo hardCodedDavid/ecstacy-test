e01dd11bfc8a2f1b5e18b98d391b9a0723402d06
Miracle Gabriel <sixpathdev@gmail.com>
2023-03-11 16:14:08 +0100

    admin lock

--- src/baseconstant.js
+++ src/baseconstant.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://127.0.0.1:8000'
-// export const BASE_URL = 'https://api.ecstasynigeria.com'
+// export const BASE_URL = 'http://127.0.0.1:8000'
+export const BASE_URL = 'https://api.ecstasynigeria.com'
 
 // server {
 //         server_name ecstasynigeria.com www.ecstasynigeria.com;
